e82ef628f3085ea1e7db834b11e51759a1f0ef78
Parksangwoo <psu8430@gmail.com>
2024-03-30 17:10:21 +0900

    feat: 11725.py 성능 개선

--- week2/11725.py
+++ week2/11725.py
@@ -6,22 +6,32 @@ input = sys.stdin.readline
 N = int(input())
 
 adj_list = [ [] for _ in range(N + 1) ]
-flag = [ False ] * (N + 1)
-p_node = [ 0 ] * (N - 1)
+# flag = [ False ] * (N + 1)
+p_node = [ 0 ] * (N + 1)
 
 for _ in range(N - 1):
   f, t = map(int, input().split())
   adj_list[f].append(t)
   adj_list[t].append(f)
 
-def dfs(n):
-  flag[n] = True
+stack = [1]
 
-  for w in adj_list[n]:
-    if not flag[w]:
-      p_node[w - 2] = n
-      dfs(w)
+while stack:
+  cur = stack.pop()
+  
+  for i in  adj_list[cur]:
+    if p_node[i] == 0:
+      p_node[i] = cur
+      stack.append(i)
 
-dfs(1)
+# def dfs(n):
+#   flag[n] = True
 
-print(*p_node, sep='\n')
+#   for w in adj_list[n]:
+#     if not flag[w]:
+#       p_node[w - 2] = n
+#       dfs(w)
+
+# dfs(1)
+
+print(*p_node[2:], sep='\n')
