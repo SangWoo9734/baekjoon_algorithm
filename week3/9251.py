c3f910172c0a410c164fac19c925092497f7c994
Parksangwoo <psu8430@gmail.com>
2024-04-06 15:11:48 +0900

    chore: 주석 추가

--- week3/9251.py
+++ week3/9251.py
@@ -13,13 +13,16 @@ def LCS(i, j):
     if i == 0 or j == 0:
       return 0
     elif X[i - 1] != Y[j - 1]:
-      # dp[i][j] = max(LCS(i - 1, j), LCS(i, j - 1))
+      # Bottom-UP
       dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-    else: # X[i] = Y[j]
+      # Top-Down
+      # dp[i][j] = max(LCS(i - 1, j), LCS(i, j - 1))
+    else: # X[i] = Y[j] 
+      # Bottom-UP
       dp[i][j] = dp[i-1][j-1]+ 1
 
+      # Top-Down
       # dp[i][j] = LCS(i - 1, j - 1) + 1
-
   return dp[i][j]
 
 # Bottom-UP
