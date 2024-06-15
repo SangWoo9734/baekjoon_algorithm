6525ab61d1ec5b9b40f7cf3408371ad26a029939
Parksangwoo <psu8430@gmail.com>
2024-06-15 09:54:45 +0900

    solve: 2232 해답 개선

--- algorithm_study/2231.py
+++ algorithm_study/2231.py
@@ -4,12 +4,17 @@ input = sys.stdin.readline
 
 N = int(input())
 
-for x in range(1, N + 1):
+small = N
+
+start_index = 0 if N - 9 * len(str(N)) < 0 else N - 9 * len(str(N))
+
+for x in range(start_index, N + 1):
   sumOfElement = sum(map(int, list(str(x))))
   if (x + sumOfElement == N):
+    small = x
     break
 
-if x == N:
+if small == N:
   print(0)
 else:
   print(x)
